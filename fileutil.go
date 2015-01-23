b82b7646d3c0f8aded31f4849e3cb67c7867ed1f
Juan Marin Otero <juan.marin.otero@gmail.com>
2015-01-23 14:21:52 -0500

    Refactor shp functions

--- fileutil.go
+++ fileutil.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"archive/zip"
-	shp "github.com/jonas-p/go-shp"
-	geojson "github.com/kpawlik/geojson"
 	"io"
 	"log"
 	"os"
-	"strings"
 )
 
 func UnzipFile(zipFile string) {
@@ -36,50 +33,4 @@ func UnzipFile(zipFile string) {
 		}
 
 	}
-}
-
-func ReadShapefile(filename string) {
-	getLines(filename)
-
-}
-
-func getLines(filename string) {
-	file, err := shp.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	fields := file.Fields()
-
-	props := make(map[string]interface{})
-
-	for file.Next() {
-		n, shape := file.Shape()
-		line := shape.(*shp.PolyLine)
-		geometry := getLineString(line)
-		for k, f := range fields {
-			name := strings.Trim(f.String(), "\u0000")
-			value := file.ReadAttribute(n, k)
-			props[name] = value
-		}
-		feature := geojson.NewFeature(geometry, props, nil)
-		json, err := geojson.Marshal(feature)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Print(json)
-
-	}
-}
-
-func getLineString(line *shp.PolyLine) *geojson.LineString {
-	points := line.Points
-	coordinates := geojson.Coordinates{}
-	for _, point := range points {
-		c := geojson.Coordinate{geojson.Coord(point.X), geojson.Coord(point.Y)}
-		coordinates = append(coordinates, c)
-	}
-	linestring := geojson.NewLineString(coordinates)
-	return linestring
 }
---
+++ shputil.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"strings"
+
+	"github.com/jonas-p/go-shp"
+	"github.com/kpawlik/geojson"
+)
+
+func ReadShapefile(filename string) {
+	getLines(filename)
+
+}
+
+func getLines(filename string) {
+	file, err := shp.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	fields := file.Fields()
+
+	props := make(map[string]interface{})
+
+	for file.Next() {
+		n, shape := file.Shape()
+		line := shape.(*shp.PolyLine)
+		geometry := getLineString(line)
+		for k, f := range fields {
+			name := strings.Trim(f.String(), "\u0000")
+			value := file.ReadAttribute(n, k)
+			props[name] = value
+		}
+		feature := geojson.NewFeature(geometry, props, nil)
+		json, err := geojson.Marshal(feature)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Print(json)
+
+	}
+}
+
+func getLineString(line *shp.PolyLine) *geojson.LineString {
+	points := line.Points
+	coordinates := geojson.Coordinates{}
+	for _, point := range points {
+		c := geojson.Coordinate{geojson.Coord(point.X), geojson.Coord(point.Y)}
+		coordinates = append(coordinates, c)
+	}
+	linestring := geojson.NewLineString(coordinates)
+	return linestring
+}
